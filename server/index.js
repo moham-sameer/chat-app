3f0b53eeb1127580aa6794f83b8a88ff8327c0c1
Mohammad Sameer <ms4492222@gmail.com>
2024-04-28 13:04:00 +0530

    troubleshoot

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('./db/connect')
 const server = http.createServer(app)
 const io = new Server(server, {
     cors:{
-        origin:"http://localhost:5173",
+        origin:"*",
         methods: ["GET", "POST"],
     }
 })
